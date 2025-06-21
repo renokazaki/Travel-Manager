16a7774ae620bf6d26d6b2aee48ddea15ed823e4
ren <ren84690@gmail.com>
2025-06-22 01:20:01 +0900

    clerkの無効化

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
-import Navigation from '@/components/navigation';
-import { ClerkProvider } from '@clerk/nextjs';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
     <html lang="ja" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
@@ -35,6 +34,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   );
 }
