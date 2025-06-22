8fec956f925562714505953db49753a1a1c761cf
ren <ren84690@gmail.com>
2025-06-22 14:00:08 +0900

    開発用コードのコメントアウト

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client/edge';
 //👷開発用
-import { config } from 'dotenv';
-config();
+// import { config } from 'dotenv';
+// config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
