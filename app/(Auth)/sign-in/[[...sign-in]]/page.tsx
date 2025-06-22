f30b3b86742499f56e46e8d1e42eb17e9315ae2a
ren <ren84690@gmail.com>
2025-06-22 12:24:45 +0900

    clerkのリダイレクト先修正

--- app/(Auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(Auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs';
 export default function Page() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center">
-      <SignIn fallbackRedirectUrl={'/'} />
+      <SignIn fallbackRedirectUrl={'/home'} />
     </div>
   );
 }
